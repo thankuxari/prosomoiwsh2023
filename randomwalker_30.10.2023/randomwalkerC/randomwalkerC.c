e0011c97e665121ecdf16d1b68c2357b3a48e0f7
thankuxari <konstantinoseyosmos@gmail.com>
2024-01-12 11:11:12 +0200

    Random Walker 30.10.2023 Part 3 Completed

---
+++ randomwalker_30.10.2023/randomwalkerC/randomwalkerC.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define STEPS 10 //Αριθμός βημάτων του περιπατητή
+#define WALKERS 1000 //Αριθμός περιπατητών
+
+int main(){
+
+    srand(time(NULL));
+
+    //Initialize file pointer
+    FILE *fptr;
+
+    //Αρχικό Position του περιπατητή
+    int STARTING = 500; 
+
+    //Άνοιγμα pointer για γράψημο στο randomwalkerA.txt αρχείο
+    fptr = fopen("randomwalkerC.txt","w");
+
+   //Loop για κάθε έναν περιπατητή
+    for(int j = 0; j < WALKERS; ++j){
+
+        //Initialise το αρχικό position του περιπατητή για κάθε έναν περιπατητή που μπαίνει στην loop
+        STARTING = 500;
+
+         //Calculate τα Steps του περιπατητή
+        for(int i = 0; i < STEPS; ++i)
+        {
+            int nextstep = rand() % 10;
+
+
+            // 0 = Αριστερά    1 = Δεξία
+            if(nextstep < 3)
+            {
+                STARTING--;
+            }else
+            {
+                STARTING++;
+            }
+
+            //Uncomment για κάθε step του περιπατητή
+            //printf("Walker:%d At step:%d: Position: %d \n",j,i,STARTING);
+        }
+
+        //Print το Final Step του περιπατητή
+        fprintf(fptr,"Walker:%d Final Position:%d \n",j,STARTING);
+    }
+    
+    return 0;
+
+}
